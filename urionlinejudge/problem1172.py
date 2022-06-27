0d017bd96594666d77d8357c2d88f42f1d2cbc3a
sizan378 <sizanmahmud08@gmail.com>
2022-06-27 15:20:54 +0600

    add some solved

---
+++ urionlinejudge/problem1172.py
@@ -0,0 +1,9 @@
+
+
+for i in range(10):
+    I = int(input())
+    if I <= 0:
+        I = 1
+        print(f'X[{i}] = {I}')
+    else:
+        print(f'X[{i}] = {I}')
---
+++ urionlinejudge/problem1173.py
@@ -0,0 +1,6 @@
+I = int(input())
+print(f'N[0] = {I}')
+for i in range(1,10):
+    I = I*2
+    print(f'N[{i}] = {I}')
+    
